a779909345d599b06d1dedf982ad52320bca99d5
Alexandr <twister_256@mail.ru>
2019-04-25 17:56:12 +0600

    Выполненный третий урок

---
+++ lesson3/tempCodeRunnerFile.js
@@ -0,0 +1,60 @@
+let money, time;
+
+function start() {
+	money = +prompt("Ваш бюджет на месяц?", '');
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+	while(isNaN(money) || money = "" || money == null){
+		money = +prompt("Ваш бюджет на месяц?", '');
+	}
+}
+
+start();
+
+
+
+
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+
+
+
+function chooseExpenses() {
+	for (let i = 0; i < 2; i++){
+		let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+			b = prompt("Во сколько обойдется?", '');
+		
+		if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+			&& a != '' && b != '' && a.length < 50) {
+			console.log("done");
+			appData.expenses[a] = b;
+		} else {                        
+			console.log ("bad result");
+			i--;
+		}
+			
+	}
+}
+
+chooseExpenses();
+
+appData.moneyPerDay = (appData.budget / 30).toFixed(1);
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100){
+	console.log("Минимальный уровень достатка")
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+	console.log("Средний уровень достатка")
+} else if (appData.moneyPerDay > 2000){
+	console.log("Высокий уровень достатка")
+} else {
+	console.log ("не тот тип данных");
+}
